deff33e9fb7281ee31c502c51498e46ccf298a82
Paul Lozada <paul_lozada@icloud.com>
2015-02-28 11:15:26 -0700

    completed strings

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,18 +11,40 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    /* WORK HERE */
+    
+    NSString *firstString = @"Cereal";
+    NSString *secondString = @"Cereal";
+    
+    string1 = firstString;
+    string2 = secondString;
+    
     return (string1 == string2);
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
+    
+  
+    NSNumber *one = [NSNumber numberWithFloat:5];
+    NSNumber *sameAsOne = [NSNumber numberWithFloat:5];
+    
+    number1 = one;
+    number2 = sameAsOne;
+    
+    
+    
+    
     return (number1 == number2);
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    /* WORK HERE */
-    return NO;
+
+    NSInteger four = 4;
+    NSInteger seven = 7;
+    
+    integer1 = seven;
+    integer2 = four;
+    
+    return integer1 != integer2;
 }
 
 @end
--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,27 +11,50 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    
+    NSString *full = cheeseName;
+    full = [cheeseName stringByReplacingOccurrencesOfString:cheeseName withString:cheeseName];
+    return full;
+    
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+
+    NSString *cheeseSuffix = @" cheese";
+    NSString *cheese = [cheeseName lowercaseString];
+    NSRange range = [cheese rangeOfString:cheeseSuffix];
+    if (range.location != NSNotFound){
+        cheese = [cheeseName substringToIndex:range.location ];
+    } else {
+        cheese = cheeseName;
+    }
+    
+   
+    return cheese;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    
+    
+    NSString *container =@"";
+    
     if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
-    } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+
+        NSString *count = [NSString stringWithFormat:@"%ld cheese",cheeseCount];
+        container = count;
+    } else  {
+    
+        
+        NSString *count = [NSString stringWithFormat:@"%ld cheeses",cheeseCount];
+        container = count;
+    }
+    return container;
+
     }
     
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
     
-    return nil;
-}
 
 @end
