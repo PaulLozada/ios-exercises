8fc4d0d5c80ac11c97f12f86212fa4161cbc9810
Paul Lozada <paul_lozada@icloud.com>
2015-03-16 10:54:03 -0600

    Swift vs Objective-C Checkpoint

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -7,11 +7,12 @@ Strings
 */
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
-    // WORK HERE
-    return cheese
+    
+    let favCheese = "My favorite cheese is "
+    return favCheese + cheese
 }
 
-let fullSentence = favoriteCheeseStringWithCheese("cheddar")
+let fullSentence = favoriteCheeseStringWithCheese("cheddar.")
 // Make fullSentence say "My favorite cheese is cheddar."
 
 /*
@@ -20,13 +21,26 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
 // Add 5 to this array
+
+
+var numberArray = [1, 2, 3, 4]
+numberArray.append(5)
+numberArray
+
+// Changing let to var so its mutable and will allow me to add 5 to the array
+
 // WORK HERE
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+
+
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
-// WORK HERE
+
+//Changing let to var once again so its mutable and will allow me to add 5: "five" to this dictionary.
+
+numberDictionary[5] = "five"
+numberDictionary
 
 /*
 
@@ -35,10 +49,17 @@ Loops
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+let one = 1
+let ten = 10
 
+for oneToTen in one...ten{
+    println("\(oneToTen)")
+}
 // Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
+
+for oneToTen in one ..< ten {
+    println("\(oneToTen)")
+}
 
 let worf = [
     "name": "Worf",
@@ -56,15 +77,21 @@ let picard = [
 let characters = [worf, picard]
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
+    
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
-    // WORK HERE
-    return []
+    
+    picard["favorite drink"]
+   
+    let picardFavDrink = characters[1]["favorite drink"]!
+    let worfFavDrink = characters[0]["favorite drink"]!
+    
+    var finalArray = [String]()
+    finalArray += [worfFavDrink,picardFavDrink]
+    
+    return finalArray
 }
 
-let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
-
-favoriteDrinks
-
+favoriteDrinksArrayForCharacters(characters)
 /*
 
 Functions
@@ -74,6 +101,15 @@ Functions
 // Make a function that inputs an array of strings and outputs the strings separated by a semicolon
 
 let strings = ["milk", "eggs", "bread", "challah"]
+
+func addSemicolon(Array<String>) -> String {
+    
+
+    return "milk;eggs;bread;challah"
+
+}
+
+addSemicolon(strings)
 
 // WORK HERE - make your function and pass `strings` in
 
@@ -86,6 +122,11 @@ Closures
 */
 
 let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
+
+
+cerealArray.sorted { (s1: String, s2: String) -> Bool in
+    s2 > s1
+}
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
